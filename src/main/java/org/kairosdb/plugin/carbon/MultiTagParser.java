d3088948e2aec817c98918d0e57a8d22c336590e
vkrish2 <vamsi.krishna@walmart.com>
2016-08-12 16:19:32 +0530

    Added MultiTagParser which 	a. Allows to define multiple tags 	b. Allows changing the case of metrics, tags to Upper/Lower 	c. Allows adding a TTL 	d. Metrics not matching the reggae, will be stored under metric name "invalidMetrics" with tag name "metricName" as the metric's name and can define a separate TTL for that

---
+++ src/main/java/org/kairosdb/plugin/carbon/MultiTagParser.java
@@ -0,0 +1,127 @@
+package org.kairosdb.plugin.carbon;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+/**
+ * Created by vkrish2 on 8/12/16.
+ */
+public class MultiTagParser implements TagParser {
+    public static final String MULTI_PATTERN_PROP = "kairosdb.carbon.multitagparser.patterns";
+    public static final String MULTI_REPLACEMENT_PROP = "kairosdb.carbon.multitagparser.replacements";
+    public static final String MULTI_TAGS = "kairosdb.carbon.multitagparser.tags";
+
+    public static final String METRIC_PATTERN_PROP = "kairosdb.carbon.multitagparser.metric_pattern";
+    public static final String METRIC_REPLACEMENT_PROP = "kairosdb.carbon.multitagparser.metric_replacement";
+
+    public static final String TAGS_CASE = "kairosdb.carbon.multitagparser.tagscase";
+
+    public static final String METRIC_TTL = "kairosdb.carbon.ttl";
+    public static final String METRIC_INVALID_TTL = "kairosdb.carbon.invalidTtl";
+
+
+    private List<Pattern> m_pattern_list = new ArrayList<Pattern>();
+    private List<String> m_replacement_list = new ArrayList<String>();
+    private List<String> m_tags_list = new ArrayList<String>();
+
+    private Pattern m_metricPattern;
+    private String m_metricReplacement;
+
+    private int m_tags_case = 0;
+    private int invalidTtl = 0;
+    private int ttl = 0;
+
+
+
+    @Inject
+    public MultiTagParser(
+            @Named(MULTI_PATTERN_PROP)String multiPatterns,
+            @Named(MULTI_REPLACEMENT_PROP)String multiReplacements,
+            @Named(MULTI_TAGS)String multiTags,
+            @Named(TAGS_CASE)String tags_case,
+            @Named(METRIC_PATTERN_PROP)String metricPattern,
+            @Named(METRIC_REPLACEMENT_PROP)String metricReplacement,
+            @Named(METRIC_INVALID_TTL)int invalidTtlValue,
+            @Named(METRIC_TTL)int ttlValue)
+    {
+
+
+        String[] m_hostPatterns = multiPatterns.split(";");
+
+        for(String m_hostPatternStr : m_hostPatterns) {
+            m_pattern_list.add(Pattern.compile(m_hostPatternStr.trim()));
+        }
+
+        m_replacement_list = Arrays.asList(multiReplacements.split(";"));
+        m_tags_list = Arrays.asList(multiTags.split(";"));
+
+        m_metricPattern = Pattern.compile(metricPattern);
+        m_metricReplacement = metricReplacement;
+
+        invalidTtl = invalidTtlValue;
+        ttl = ttlValue;
+
+
+        if (tags_case.equalsIgnoreCase("upper")){
+            m_tags_case = 1;
+        }
+        else if (tags_case.equalsIgnoreCase("lower")){
+            m_tags_case = 2;
+        }
+    }
+
+    @Override
+    public CarbonMetric parseMetricName(String metricName)
+    {
+
+        switch(m_tags_case){
+            case 1:
+                metricName = metricName.toUpperCase();
+                break;
+            case 2:
+                metricName = metricName.toLowerCase();
+                break;
+            default:
+                break;
+        }
+
+        Matcher metricMatcher = m_metricPattern.matcher(metricName);
+        CarbonMetric ret;
+
+        if(!metricMatcher.matches()){
+            ret = new CarbonMetric("invalidMetrics");
+            ret.addTag("metricName", metricName);
+            ret.setTtl(invalidTtl);
+            return (ret);
+        }
+
+        Matcher hostMatcher;
+        ret = new CarbonMetric(metricMatcher.replaceAll(m_metricReplacement));
+
+        Iterator<Pattern> patternIterator = m_pattern_list.iterator();
+        Iterator<String> replacementIterator = m_replacement_list.iterator();
+        Iterator<String> tagsIterator = m_tags_list.iterator();
+
+        while(patternIterator.hasNext() && replacementIterator.hasNext() && tagsIterator.hasNext()){
+            hostMatcher = patternIterator.next().matcher(metricName);
+            if (!hostMatcher.matches()){
+                ret = new CarbonMetric("invalidMetrics");
+                ret.addTag("metricName", metricName);
+                ret.setTtl(invalidTtl);
+                return (ret);
+            }
+            ret.addTag(tagsIterator.next().trim(), hostMatcher.replaceAll(replacementIterator.next().trim()));
+        }
+
+        ret.setTtl(ttl);
+        return (ret);
+    }
+}
--- src/main/java/org/kairosdb/plugin/carbon/pickle/Unpickler.java
+++ src/main/java/org/kairosdb/plugin/carbon/pickle/Unpickler.java
@@ -3,6 +3,8 @@ package org.kairosdb.plugin.carbon.pickle;
 import net.razorvine.pickle.Opcodes;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 
 /**
  Created with IntelliJ IDEA.
@@ -14,6 +16,8 @@ import java.io.IOException;
 public class Unpickler extends net.razorvine.pickle.Unpickler
 {
 	private boolean m_firstTuple = true;
+	static int i=0;
+	private boolean is_tuple = false;
 
 	@Override
 	protected void dispatch(short key) throws IOException
@@ -38,6 +42,55 @@ public class Unpickler extends net.razorvine.pickle.Unpickler
 			}
 			else
 				m_firstTuple = false;
+		}
+		else if ((key == Opcodes.TUPLE))
+		{
+
+			i++;
+			System.out.println("i = " + i);
+			if(i%2 == 0){
+				i =0 ;
+				return ;
+			}
+
+			if (m_firstTuple)
+			{
+
+				m_firstTuple = true;
+				//Pop three items from stack
+				Object value = stack.pop();
+				long time = ((Number)stack.pop()).longValue();
+				stack.pop();
+				String path = /*(String)*/stack.pop().toString();
+
+
+				PickleMetric metric;
+				if (/*value instanceof Double*/ value.toString().contains(".")){
+					metric = new PickleMetric(path, time, Double.parseDouble(value.toString()));
+				}
+				else{
+					metric = new PickleMetric(path, time, Long.parseLong(value.toString()));
+				}
+
+				stack.add(metric);
+
+			}
+			else{
+				m_firstTuple = false;
+			}
+
+			is_tuple = true;
+		}
+		else if(key == Opcodes.APPEND && is_tuple){
+			is_tuple = false;
+			Object value = stack.pop();
+			stack.pop();
+
+			@SuppressWarnings("unchecked")
+			ArrayList<Object> list = (ArrayList<Object>) stack.peek();
+
+			list.add(value);
+
 		}
 		else
 			super.dispatch(key);
